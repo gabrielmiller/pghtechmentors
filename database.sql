5970639d4adfc1059137d23c9f551937adbfe53b
L Gabriel Miller <gmiller2007@gmail.com>
2013-06-02 14:48:58 -0400

    Renames user table column password to passwd. Adds user table column salt.

--- database.sql
+++ database.sql
@@ -1,18 +1,26 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 4.0.2
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 02, 2013 at 02:08 PM
--- Server version: 5.5.24-log
--- PHP Version: 5.3.13
+-- Generation Time: Jun 02, 2013 at 06:43 PM
+-- Server version: 5.5.31
+-- PHP Version: 5.3.25
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `pghtechmentors`
 --
+CREATE DATABASE IF NOT EXISTS `pghtechmentors` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `pghtechmentors`;
 
 -- --------------------------------------------------------
 
@@ -31,35 +39,13 @@ CREATE TABLE IF NOT EXISTS `day` (
 --
 
 INSERT INTO `day` (`day_id`, `day_name`) VALUES
-(1, 'Sunday'),
-(2, 'Monday'),
-(3, 'Tuesday'),
-(4, 'Wednesday'),
-(5, 'Thursday'),
-(6, 'Friday'),
-(7, 'Saturday');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `location`
---
-
-CREATE TABLE IF NOT EXISTS `location` (
-  `locationid` int(11) NOT NULL,
-  `locationname` varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `skill`
---
-
-CREATE TABLE IF NOT EXISTS `skill` (
-  `skill_id` int(11) NOT NULL,
-  `skill_name` varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+(1, 'Monday'),
+(2, 'Tuesday'),
+(3, 'Wednesday'),
+(4, 'Thursday'),
+(5, 'Friday'),
+(6, 'Saturday'),
+(7, 'Sunday');
 
 -- --------------------------------------------------------
 
@@ -99,21 +85,21 @@ CREATE TABLE IF NOT EXISTS `user` (
   `skill` varchar(255) NOT NULL,
   `is_available` tinyint(1) NOT NULL DEFAULT '0',
   `zip_code` varchar(10) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `about_me` text NOT NULL,
+  `passwd` varchar(255) NOT NULL,
+  `salt` varchar(255) NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `email_id` (`email_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
--- Table structure for table `user_location`
+-- Dumping data for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user_location` (
-  `user_id` int(11) NOT NULL,
-  `location_id` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+INSERT INTO `user` (`user_id`, `account_type`, `email_id`, `name_last`, `name_first`, `contact_home`, `contact_mobile`, `skill`, `is_available`, `zip_code`, `about_me`, `passwd`, `salt`) VALUES
+(1, 'M', 'mentor@gmail.com', 'Mentor ', 'Bob', NULL, '12132432', 'Web Development', 1, '15217', 'I have 11 yrs of experience in the Web site development. I love to teach kids. My prev. exp is so and so. ', '12345', ''),
+(2, 'F', 'facilitator@pitt.com', 'Bathney house', '', '32323', NULL, '', 1, '15225', 'We are Bathney House', '', ''),
+(3, 'M', 'gabe@miller.com', 'Miller', 'Gabe', '123445689', '123', 'Web Development', 1, '11111', 'Hi!!!!', 'e9cff9796f470b0272406a68ade7086b45e66152', '3b139da640');
 
 -- --------------------------------------------------------
 
@@ -129,17 +115,6 @@ CREATE TABLE IF NOT EXISTS `user_privilege` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_skill`
---
-
-CREATE TABLE IF NOT EXISTS `user_skill` (
-  `user_id` int(11) NOT NULL,
-  `skill_id` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `user_timeslot`
 --
 
@@ -148,3 +123,15 @@ CREATE TABLE IF NOT EXISTS `user_timeslot` (
   `day_id` int(11) NOT NULL,
   `timeslot_id` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_timeslot`
+--
+
+INSERT INTO `user_timeslot` (`user_id`, `day_id`, `timeslot_id`) VALUES
+(1, 1, 1),
+(1, 2, 1);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
